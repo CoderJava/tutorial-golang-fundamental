92d2e1210ef419e378de12297428cdbaf38ca5fc
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-08-09 21:35:25 +0700

    Kuis 1

--- main.go
+++ main.go
@@ -8,4 +8,7 @@ import (
 func main() {
 	resultAdd := calculation.Add(5, 8)
 	fmt.Println("result add:", resultAdd)
+
+	resultMultiply := calculation.Multiply(4, 4)
+	fmt.Println("result multiply:", resultMultiply)
 }
