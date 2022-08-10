45847d7d4f4f4297f4a986f4cd99ddd79ff9f9b0
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-08-10 20:30:35 +0700

    Belajar if conditional

--- main.go
+++ main.go
@@ -3,13 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	// Cara pendeklarasian variable
-	// 1. Buat variable tanpa di-assign
-	var name string
-	name = "Pablo"
-	fmt.Println(name)
+	// if else
+	/* age := 28
+	if age > 10 {
+		fmt.Println("Boleh bermain game")
+	} else {
+		fmt.Println("Tidak boleh bermain game")
+	} */
 
-	// 2. Buat variable langsung di-assign
-	age := 28
-	fmt.Println(age)
+	// else if
+	score := 80
+	var grade string
+	if score <= 50 {
+		grade = "E"
+	} else if score <= 60 {
+		grade = "D"
+	} else if score <= 70 {
+		grade = "C"
+	} else if score <= 85 {
+		grade = "B"
+	} else {
+		grade = "A"
+	}
+	fmt.Println("grade:", grade)
 }
