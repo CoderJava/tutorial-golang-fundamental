290b4ec4f5d1519aa2485750b98d714e72074ce2
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-08-11 20:27:46 +0700

    Belajar basic function

--- main.go
+++ main.go
@@ -3,29 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	scores := [8]int{
-		100,
-		80,
-		75,
-		92,
-		70,
-		93,
-		88,
-		67,
-	}
+	sentence := printMyResult("Saya sedang")
+	fmt.Println(sentence)
+}
 
-	// hitung rata-rata
-	// masukkan nilai >= 90 kedalam slice goodScores
-	average := 0.0
-	var goodScores []int
-	for _, itemScore := range scores {
-		average += float64(itemScore)
-		if itemScore >= 90 {
-			goodScores = append(goodScores, itemScore)
-		}
-	}
-	average = average / float64(len(scores))
-	fmt.Println("average:", average)
-	fmt.Println("good scores:", goodScores)
-
+func printMyResult(sentence string) string {
+	newSentence := sentence + " belajar Golang"
+	return newSentence
 }
