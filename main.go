ba1d730cafbb8c765d25376f9e77ec634d2ff68a
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-08-11 20:43:53 +0700

    Belajar function dengan multiple return

--- main.go
+++ main.go
@@ -3,10 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	result := add(10, 20)
-	fmt.Println(result)
+	luas, keliling := calculate(5, 4)
+	fmt.Println("luas:", luas, "keliling:", keliling)
+
+	luas2, keliling2 := calculate2(3, 5)
+	fmt.Println("luas:", luas2, "keliling:", keliling2)
 }
 
-func add(numberOne int, numberTwo int) int {
-	return numberOne + numberTwo
+func calculate(panjang int, lebar int) (int, int) {
+	luas := panjang * lebar
+	keliling := 2 * (panjang + lebar)
+	return luas, keliling
+}
+
+func calculate2(panjang int, lebar int) (luas int, keliling int) {
+	luas = panjang * lebar
+	keliling = 2 * (panjang + lebar)
+	return luas, keliling
 }
