a79cfb6145d872fa9a1f6b454ccf761b04db042c
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-08-11 08:37:52 +0700

    Belajar map lebih lanjut

--- main.go
+++ main.go
@@ -14,8 +14,22 @@ func main() {
 	// fmt.Println(myMap["go"])
 
 	myMap := map[string]string{
-		"ruby": "is beautiful",
-		"go": "is super fast",
+		"ruby":       "is beautiful",
+		"go":         "is super fast",
+		"JavaScript": "hype",
 	}
-	fmt.Println(myMap)
+	for key, value := range myMap {
+		fmt.Println("Key:", key, "Value:", value)
+	}
+
+	fmt.Println("==================")
+
+	delete(myMap, "ruby")
+	for key, value := range myMap {
+		fmt.Println("Key:", key, "Value:", value)
+	}
+
+	value, isAvailable := myMap["python"]
+	fmt.Println(value)
+	fmt.Println(isAvailable)
 }
