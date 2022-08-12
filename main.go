27fe5b855165e044afff3c3fcc3f78e12f1616de
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-08-12 20:40:43 +0700

    Belajar embedded struct

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ func main() {
 	user.LastName = "Setiawan"
 	user.Email = "kolonel.yudisetiawan@gmail.com"
 	user.IsActive = true
-
 	user2 := User{
 		ID:        2,
 		FirstName: "Link",
@@ -17,15 +16,20 @@ func main() {
 		Email:     "link@nintendo.com",
 		IsActive:  false,
 	}
-
 	user3 := User{3, "Eka", "Pablo", "eka@gmail.com", false}
 
-	resultUser1 := displayUser(user)
-	resultUser2 := displayUser(user2)
-	resultUser3 := displayUser(user3)
-	fmt.Println(resultUser1)
-	fmt.Println(resultUser2)
-	fmt.Println(resultUser3)
+	users := []User{
+		user,
+		user2,
+		user3,
+	}
+	group := Group{
+		Name:        "Gamer",
+		Admin:       user,
+		Users:       users,
+		IsAvailable: true,
+	}
+	displayGroup(group)
 }
 
 type User struct {
@@ -36,7 +40,25 @@ type User struct {
 	IsActive  bool
 }
 
-func displayUser(user User) string {
-	result := fmt.Sprintf("Name: %s %s, Email: %s", user.FirstName, user.LastName, user.Email)
-	return result
+type Group struct {
+	Name        string
+	Admin       User
+	Users       []User
+	IsAvailable bool
+}
+
+// func displayUser(user User) string {
+// 	output := fmt.Sprintf("Name: %s %s, Email: %s", user.FirstName, user.LastName, user.Email)
+// 	return output
+// }
+
+func displayGroup(group Group) {
+	fmt.Printf("Name: %s", group.Name)
+	fmt.Println()
+	fmt.Printf("Member count: %d", len(group.Users))
+	fmt.Println()
+
+	for _, user := range group.Users {
+		fmt.Println(user.FirstName)
+	}
 }
