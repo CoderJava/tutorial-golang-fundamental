259c3145c0187a966e32d1022b5bb56fea3c9121
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-08-13 11:31:20 +0700

    Belajar pointer struct sebagai parameter

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	number := 5
-	fmt.Println(number)
-	change(&number, 100)
-	fmt.Println(number)
+	student := Student{
+		ID:   1,
+		Name: "Agung Setiawan",
+		GPA:  0,
+	}
+	fmt.Println(student.Name)
+	graduate(&student)
+	fmt.Println(student.Name)
 }
 
-func change(old *int, new int) {
-	*old = new
-	fmt.Println("didalam function:", *old)
+type Student struct {
+	ID   int
+	Name string
+	GPA  float32
+}
+
+func graduate(student *Student) {
+	student.Name = student.Name + " S.T"
 }
