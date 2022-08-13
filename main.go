1b5eeb26064821f57693aa225a068e11e8470819
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-08-13 11:37:13 +0700

    Belajar method dengan pointer receiver

--- main.go
+++ main.go
@@ -6,11 +6,20 @@ func main() {
 	student := Student{
 		ID:   1,
 		Name: "Agung Setiawan",
-		GPA:  0,
+		GPA:  3.7,
+	}
+	var student2 *Student = &Student{
+		ID:   2,
+		Name: "Eko Kurniawan",
+		GPA:  3.8,
 	}
 	fmt.Println(student.Name)
-	graduate(&student)
+	student.graduate()
 	fmt.Println(student.Name)
+	fmt.Println("====================")
+	fmt.Println(student2.Name)
+	student2.graduate()
+	fmt.Println(student2.Name)
 }
 
 type Student struct {
@@ -19,6 +28,6 @@ type Student struct {
 	GPA  float32
 }
 
-func graduate(student *Student) {
+func (student *Student) graduate() {
 	student.Name = student.Name + " S.T"
 }
