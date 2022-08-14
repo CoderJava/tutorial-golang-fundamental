ff187c1f359ff92cda1fc5de71572ceabdd8524f
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-08-14 09:10:07 +0700

    Masalah jika kita tidak pakai interface

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
+import "fmt"
+
 func main() {
-
+	persegi := Persegi{
+		Sisi: 4,
+	}
+	persegiPanjang := PersegiPanjang{
+		Panjang: 6,
+		Lebar:   5,
+	}
+	luasPersegi := SeberapaLuas1(persegi)
+	luasPersegiPanjang := SeberapaLuas2(persegiPanjang)
+	fmt.Println("luas persegi:", luasPersegi)
+	fmt.Println("luas persegi panjang:", luasPersegiPanjang)
 }
 
-type Luas interface {
-	HitungLuas() int
+// Masalah jika tidak pakai interface maka, kita harus membuat satu persatu function SeberapaLuas sesuai
+// dengan parameteter struct-nya masing-masing.
+func SeberapaLuas1(persegi Persegi) int {
+	return persegi.Sisi
 }
+
+func SeberapaLuas2(persegiPanjang PersegiPanjang) int {
+	return persegiPanjang.HitungLuas()
+}
+
+// type Luas interface {
+// 	HitungLuas() int
+// }
 
 type Persegi struct {
 	Sisi int
