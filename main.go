7b608b31c5628dedbae55ca5bf406ead2a40561f
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-08-14 08:57:45 +0700

    Belajar interface basic

--- main.go
+++ main.go
@@ -1,27 +1,26 @@
 package main
 
-import "fmt"
-
 func main() {
-	gamer := Gamer{
-		Name:  "Yudi Setiawan",
-		Games: []string{},
-	}
-	gamer.AddGame("Tekken 7")
-	gamer.AddGame("Fifa 2022")
-	gamer.AddGame("The last of us")
-	gamer.AddGame("The last of us part 2")
 
-	for _, game := range gamer.Games {
-		fmt.Println(game)
-	}
 }
 
-func (gamer *Gamer) AddGame(game string) {
-	gamer.Games = append(gamer.Games, game)
+type Luas interface {
+	HitungLuas() int
 }
 
-type Gamer struct {
-	Name  string
-	Games []string
+type Persegi struct {
+	Sisi int
+}
+
+func (persegi Persegi) HitungLuas() int {
+	return persegi.Sisi * persegi.Sisi
+}
+
+type PersegiPanjang struct {
+	Panjang int
+	Lebar   int
+}
+
+func (persegiPanjang PersegiPanjang) HitungLuas() int {
+	return persegiPanjang.Panjang * persegiPanjang.Lebar
 }
